38dfca3b0986b85a086b5631692fc97b6465f57b
Mosdevv <vikentorpierre@gmail.com>
2019-09-18 21:15:15 -0400

    Updates

---
+++ precache-manifest.370141752dba4edcc63b3343c8610def.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca1a59832167abf663d5d668685ca348",
+    "url": "/public_portfolio/index.html"
+  },
+  {
+    "revision": "3b7fb4a22abbfb23118c",
+    "url": "/public_portfolio/static/css/main.9301ba16.chunk.css"
+  },
+  {
+    "revision": "48fc745a06abf101f103",
+    "url": "/public_portfolio/static/js/2.9d37fe12.chunk.js"
+  },
+  {
+    "revision": "3b7fb4a22abbfb23118c",
+    "url": "/public_portfolio/static/js/main.998f0093.chunk.js"
+  },
+  {
+    "revision": "4de0e198ac5d426a447a",
+    "url": "/public_portfolio/static/js/runtime~main.d722a053.js"
+  },
+  {
+    "revision": "e63f45b63511031847fe26b050ae7474",
+    "url": "/public_portfolio/static/media/apex.e63f45b6.png"
+  },
+  {
+    "revision": "9e4f61c77a287b620a38c14a8870fbcf",
+    "url": "/public_portfolio/static/media/gitIcon.9e4f61c7.png"
+  },
+  {
+    "revision": "7632c7822795550ec5eac7a7a8ffcfb6",
+    "url": "/public_portfolio/static/media/jsImg2.7632c782.png"
+  },
+  {
+    "revision": "ead37a929e598d0f2d3b87f0aff89f1b",
+    "url": "/public_portfolio/static/media/reactIcon2.ead37a92.png"
+  },
+  {
+    "revision": "557f88e71724e9ed90445e030706a3b4",
+    "url": "/public_portfolio/static/media/resume.557f88e7.pdf"
+  },
+  {
+    "revision": "6d0f2c886806819092229cf80ec91ad2",
+    "url": "/public_portfolio/static/media/vespo.6d0f2c88.png"
+  }
+]);
