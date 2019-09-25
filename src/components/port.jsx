9fdd20ee20c4ef936de265b72f16ddac0ac66678
Mosdevv <vikentorpierre@gmail.com>
2019-09-24 23:20:48 -0400

    added loneme port section

--- src/components/port.jsx
+++ src/components/port.jsx
@@ -1,12 +1,51 @@
 import React from "react";
 import pic01 from "../img/apex.png";
 import pic02 from "../img/vespo.png";
+import pic03 from "../img/loneme.png";
 const Portfilio = () => {
   return (
     <div className="inner">
       <header class="header portfolio">
         <h2> portfolio </h2>
       </header>
+      <article class="feature left">
+        <span class="image">
+          <img className="feature--img " src={pic02} alt="" />
+        </span>
+        <div class="content">
+          {/* <h2>Integer vitae libero acrisus egestas placerat sollicitudin</h2> */}
+          <p>
+            A landing Page for a start-up located in Orlando. Started at the University Of Central Florida.
+            Vespo is a customer business reward system. customer shop at any vendor and earn rewards.
+          </p>
+          <div>
+            <a href="https://vespolandingpage.herokuapp.com" target="_blank" className="link-src">
+              View Site
+              </a>
+            <a href="https://github.com/VikentorPierre/Vespo-landing-page" target="_blank" className="link-src" > GitHub </a>
+          </div>
+        </div>
+      </article>
+      <article class="feature right">
+        <span class="image">
+          <img className="feature--img" src={pic03} alt="" />
+        </span>
+        <div class="content">
+          {/* <h2>Integer vitae libero acrisus egestas placerat sollicitudin</h2> */}
+          <p>
+            loneme is a full stack blog post side am currently working on. please note this sample is using MomngoDB atlas free version.
+            so the backend database IP will keep on changing. So authentication and authorization is fully working, but when atlas change IP it stop working.
+            Even so, the site is fully working with the abillity to create new posts, comment, like, and sign-up and login.
+          </p>
+          <div>
+            <a href="https://vikentorpierre.github.io/lonme_01/" target="_blank" className="link-src">
+              View Site
+              </a>
+            <a href="https://github.com/VikentorPierre/lonme_01" target="_blank" className="link-src" > GitHub </a>
+          </div>
+
+        </div>
+      </article>
       <article class="feature left">
         <span class="image">
           <img className="feature--img " src={pic01} alt="" />
@@ -27,33 +66,6 @@ const Portfilio = () => {
               </a>
             <a href="https://github.com/VikentorPierre/ApexLegendAPITracker" target="_blank" className="link-src" > GitHub </a>
           </div>
-          {/* <ul class="actions">
-            <li>
-              <a href="https://apextrackerapi.herokuapp.com/" class="button alt">
-                Site
-              </a>
-              <a href=""> GitHub </a>
-            </li>
-          </ul> */}
-        </div>
-      </article>
-      <article class="feature right">
-        <span class="image">
-          <img className="feature--img" src={pic02} alt="" />
-        </span>
-        <div class="content">
-          {/* <h2>Integer vitae libero acrisus egestas placerat sollicitudin</h2> */}
-          <p>
-            A landing Page for a start-up located in Orlando. Started at the University Of Central Florida.
-            Vespo is a customer business reward system. customer shop at any vendor and earn rewards.
-          </p>
-          <div>
-            <a href="https://vespolandingpage.herokuapp.com" target="_blank" className="link-src">
-              View Site
-              </a>
-            <a href="https://github.com/VikentorPierre/Vespo-landing-page" target="_blank" className="link-src" > GitHub </a>
-          </div>
-
         </div>
       </article>
     </div>
