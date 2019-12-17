f4c2a69b832de6dd1d31eca94de21cfe72d1a0cf
vikentorpierre <vikentorpierre@gmail.com>
2019-12-16 19:39:34 -0800

    added angular and php as skill

--- src/components/skill.jsx
+++ src/components/skill.jsx
@@ -1,46 +1,53 @@
-import React from "react";
-import "../css/home.css";
-import pythonImg from "../img/pythomImg.png";
-import nodeImg from "../img/nodeIcon2.png";
-import react from "../img/reactIcon2.png";
-import figma from "../img/figmaIcon2.png";
-import js from "../img/jsImg2.png";
-import git from "../img/gitIcon.png";
-import wp from "../img/wordpress.png";
+import React from 'react';
+import '../css/home.css';
+import pythonImg from '../img/pythomImg.png';
+import nodeImg from '../img/nodeIcon2.png';
+import react from '../img/reactIcon2.png';
+import figma from '../img/figmaIcon2.png';
+import js from '../img/jsImg2.png';
+import git from '../img/gitIcon.png';
+import wp from '../img/wordpress.png';
+import angular from '../img/angular.png';
+import php from '../img/php.jpg';
 
 const Skills = () => {
   return (
-    <div class="inner">
-      <header class="header skill">
+    <div class='inner'>
+      <header class='header skill'>
         <h2> Skills </h2>
         {/* <h3>
           4+ years
         </h3> */}
       </header>
-      <div class="image-grid">
-        <div className="img">
-          <img className="img-size" src={react} alt="reactjs" />
+      <div class='image-grid'>
+        <div className='img'>
+          <img className='img-size' src={react} alt='reactjs' />
         </div>
-        <div className="img">
-          <img className="img-size" src={wp} alt=" wordpress " />
+        <div className='img'>
+          <img className='img-size' src={wp} alt=' wordpress ' />
         </div>
-        <div className="img">
-          <img height="150px" width="150px" src={js} alt="Javascript" />
+        <div className='img'>
+          <img height='150px' width='150px' src={js} alt='Javascript' />
         </div>
-        <div className="img">
-          <img className="img-size" src={pythonImg} alt="python" />
+        <div className='img'>
+          <img className='img-size' src={pythonImg} alt='python' />
         </div>
-        <div className="img">
-          <img className="img-size" src={git} alt="git and github" />
+        <div className='img'>
+          <img className='img-size' src={git} alt='git and github' />
         </div>
-        <div className="img">
-          <img className="img-size" src={figma} alt="figma" />
+        <div className='img'>
+          <img className='img-size' src={figma} alt='figma' />
         </div>
-        <div className="img">
-          <img className="img-size" src={nodeImg} alt=" nodejs " />
+        <div className='img'>
+          <img className='img-size' src={nodeImg} alt=' nodejs ' />
+        </div>
+        <div className='img'>
+          <img className='img-size' src={angular} alt=' angular ' />
+        </div>
+        <div className='img'>
+          <img className='img-size' src={php} alt=' php ' />
         </div>
       </div>
-
     </div>
   );
 };
